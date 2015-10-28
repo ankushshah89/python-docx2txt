75b91b1d0be430b9f85120ed3abb86f9ffb9c38a
Ankush Shah <ankush.shah.nitk@gmail.com>
2015-10-28 17:42:25 +0530

    next version

--- docx2txt/__init__.py
+++ docx2txt/__init__.py
@@ -1,3 +1,3 @@
 from docx2txt import process
 
-VERSION = "0.1"
+VERSION = "0.2"
--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ scripts = glob.glob("bin/*")
 setup(
   name = 'docx2txt',
   packages = ['docx2txt'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A pure python-based utility to extract text from docx files.',
   author = 'Ankush Shah',
   author_email = 'ankush.shah.nitk@gmail.com',
   url = 'https://github.com/ankushshah89/python-docx2txt', # use the URL to the github repo
-  download_url = 'https://github.com/ankushshah89/python-docx2txt/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/ankushshah89/python-docx2txt/tarball/0.2', # I'll explain this in a second
   keywords = ['python', 'docx', 'text'], # arbitrary keywords
   scripts = scripts,
   classifiers = [],
