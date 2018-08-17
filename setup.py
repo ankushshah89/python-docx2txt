334b3f89e7cbd23d152a1f655c28bab106bc2c3e
Andrew Champion <awchampion@gmail.com>
2018-08-17 16:35:05 -0700

    update installed version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ scripts = glob.glob('bin/*')
 setup(
   name='docx2txt',
   packages=['docx2txt'],
-  version='0.7',
+  version='0.8',
   description='A pure python-based utility to extract text and images '
               'from docx files.',
   author='Ankush Shah',
