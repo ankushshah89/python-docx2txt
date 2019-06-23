c94663234d2882aa75932f9c9973eb5a804df13b
ankushshah <ankushshah@google.com>
2019-06-23 12:53:28 -0700

    version++

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ scripts = glob.glob('bin/*')
 setup(
   name='docx2txt',
   packages=['docx2txt'],
-  version='0.7',
+  version='0.8',
   description='A pure python-based utility to extract text and images '
               'from docx files.',
   author='Ankush Shah',
   author_email='ankush.shah.nitk@gmail.com',
   url='https://github.com/ankushshah89/python-docx2txt',
-  download_url='https://github.com/ankushshah89/python-docx2txt/tarball/0.7',
+  download_url='https://github.com/ankushshah89/python-docx2txt/tarball/0.8',
   keywords=['python', 'docx', 'text', 'images', 'extract'],
   scripts=scripts,
   classifiers=[],
